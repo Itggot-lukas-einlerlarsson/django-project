1eb2d215495df99d0a652f08a7f93d0b1980aa4d
Lukas E.Larsson <lukas.einlerlarsson@itggot.com>
2024-01-09 18:23:14 +0100

    added book search

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "base.html")
+    name = request.GET.get("name") or "world"
+    return render(request, "base.html", {"name" : name})
+
+def book_search(request):
+    search_text = request.GET.get("search") or "world"
+    return render(request, "book_search.html", {"search_text" : search_text})
