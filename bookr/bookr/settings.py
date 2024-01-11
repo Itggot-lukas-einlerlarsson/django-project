b708bd1c726787a5a7dcf17729a464ffcf9b30f0
Lukas E.Larsson <lukas.einlerlarsson@itggot.com>
2024-01-11 19:52:27 +0100

    at Django’s database CRUD operations

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'bookr.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
+}
+
+DJANGO_PG_JSONSCHEMA = {
+    'PG_COMMIT_JSONSCHEMA': True
 }
 
 
